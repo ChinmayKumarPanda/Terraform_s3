1d8fba1b2916c5eb359d245eeb39680c8842641f
chinu <chinmaypanda447@gmail.com>
2024-08-26 23:35:50 +0530

    update main file

--- main.tf
+++ main.tf
@@ -4,3 +4,14 @@ tags={
     names= "first"
 }  
 }
+resource "aws_s3_bucket_acl" "versioning_bucket_acl" {
+  bucket = aws_s3_bucket.versioning_bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.versioning_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
