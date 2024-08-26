a66987ec1d2c5f31c2ed65ca744787e9b9607eef
chinu <chinmaypanda447@gmail.com>
2024-08-26 23:57:54 +0530

    updated .gitignore

--- main.tf
+++ main.tf
@@ -4,13 +4,9 @@ tags={
     names= "first"
 }  
 }
-resource "aws_s3_bucket_acl" "versioning_bucket_acl" {
-  bucket = aws_s3_bucket.versioning_bucket.id
-  acl    = "private"
-}
 
 resource "aws_s3_bucket_versioning" "versioning" {
-  bucket = aws_s3_bucket.versioning_bucket.id
+  bucket = aws_s3_bucket.bucket_1.id 
   versioning_configuration {
     status = "Enabled"
   }
